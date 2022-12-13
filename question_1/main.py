339dbacb7aed64b0fc1d7a3aa426eee96f9fed05
Tal Zichlinsky <talz@ip-com.co.il>
2022-12-14 00:09:43 +0200

    Fix Q1

--- question_1/main.py
+++ question_1/main.py
@@ -4,7 +4,7 @@ import numpy as np
 import cvxpy as cp
 from plot import plot_times
 
-TEST_SIZE = 150
+TEST_SIZE = 10
 
 def time_numpy(max_size):
     times = []
@@ -16,24 +16,23 @@ def time_numpy(max_size):
         times.append(diff)
     return times
 
+
 def time_cvxpy(max_size):
     times = []
     for n in range(1, max_size+1):
         (A, b) = equations.numpy_linear_eq(n, n, 1, 1000)
-        x = cp.Variable(n)
-        objective = cp.Minimize(cp.)
-        prob = cp.Problem(objective, [A @ x == b])
+        x = cp.Variable(A.shape)
+        y = x - b
+        objective = cp.Minimize(cp.sum_squares(A @ y))
+        problem = cp.Problem(objective, [])
         t1 = time.time()
-        prob.solve()
+        problem.solve()
         diff = time.time() - t1
         times.append(diff)
     return times
 
-times = time_numpy(TEST_SIZE)
+times_np = time_numpy(TEST_SIZE)
+times_cp = time_cvxpy(TEST_SIZE)
 input_sizes = [i for i in range(1, TEST_SIZE+1)]
-plot_times(input_sizes, times)
-
-
-times = time_cvxpy(TEST_SIZE)
-input_sizes = [i for i in range(1, TEST_SIZE+1)]
+times = {'np': times_np, 'cp': times_cp}
 plot_times(input_sizes, times)
--- question_1/plot.py
+++ question_1/plot.py
@@ -4,12 +4,14 @@ import matplotlib.pyplot as plt
 
 def plot_times(input_sizes, times):
     x = np.array(input_sizes)
-    y = np.array(times)
+    y_np = np.array(times['np'])
+    y_cp = np.array(times['cp'])
     
     _, ax = plt.subplots()
-    ax.plot(x, y)
+    ax.plot(x, y_np, color='r', label='numpy')
+    ax.plot(x, y_cp, color='b', label='cvxpy')
     ax.set(xlabel='input size', ylabel='time (s)',
        title='Run Time')
     ax.grid()
-
+    ax.legend()
     plt.show()
